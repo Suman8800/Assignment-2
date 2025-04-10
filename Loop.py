a2819fc4120987f456e68e529c108134a1282334
Suman8800 <117619622+Suman8800@users.noreply.github.com>
2025-04-10 22:09:16 +0530

    Add files via upload

---
+++ Loop.py
@@ -0,0 +1,8 @@
+def main():
+    total_sum = 0
+    for number in range(1, 51):
+        total_sum += number
+    print(f"The sum of all integers from 1 to 50 is: {total_sum}")
+
+if __name__ == "__main__":
+    main()
---
+++ odd or even.py
@@ -0,0 +1,5 @@
+A= int(input('Enter a number: '))
+if A % 2 == 0:
+    print('Even')
+else:
+    print('Odd')
